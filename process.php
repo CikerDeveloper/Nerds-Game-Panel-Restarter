9ed83a02f896a99fe1cf4ce028d770fec8958ab9
RootSec <itznaktuminajasiftw@gmail.com>
2019-02-15 20:14:44 +0100

    Update process.php

--- process.php
+++ process.php
@@ -2,34 +2,26 @@
 include "main.php";
 $br_skeniranja = $config['broj_skeniranja'];
 
-if ($_POST['task']=="gtstatus") {
-	$ip = $restarter->ocistiIP($_POST['ip']);
-	$server = $restarter->skenirajServer($ip);
+if ($_POST['task']=="status") {
+	$server = $restarter->skenirajServer($serverid);
 
-	if (!$server) {
-		$return['status'] = "No server";
-		$return['players'] = "-";
-		$return['action'] = "hide";
+	if($server=="Offline")
+	{
+		return "Offline";
 	} else {
-		$return['status'] = $server['status'];
-		if (!$return['status']) $return['players'] = "-";
-		else $return['players'] = $server['players']."/".$server['playersmax'];
-		$return['action'] = "display";
+		return "Online";
 	}
-
-	die(json_encode($return));
 
 } else if ($_POST['task']=="restartServer") {
 	$serverid = (int)$_POST['serverid'];
-	$ip = $restarter->ocistiIP($_POST['ip']);
 	$temp = (int)$_POST['auth'];
 	$token = $config['tokens'][$temp];
-	$server = $restarter->skenirajServer($ip);
+	$server = $restarter->skenirajServer($serverid);
 
 	if (!$server || in_array($serverid, $config['ignorisi'])) {
 		$return['msg'] = "No server";
 	} else {
-		if ($server['status']) {
+		if ($server=="Online") {
 			$return['msg'] = "Server online";
 		} else {
 			$return['msg'] = $restarter->restartujServer($serverid, $token);
