1fdc1c3a20809dae807112c9c89f9a248956e99c
RootSec <itznaktuminajasiftw@gmail.com>
2019-02-15 19:48:49 +0100

    Update restarter.class.php

--- assets/classes/restarter.class.php
+++ assets/classes/restarter.class.php
@@ -1,141 +1,45 @@
 <?php
 
 class Restarter {
-
+	
+	//kolko loš KGB API ZA RESTARTER NAPISAO U 2 FUNKCIJE ONI U 50..LOL
+	//sad je lepše
 	/*
-	 * Metoda poziva parsiranje, i vraca listu servera u nizu
-	 */
-	function getServers() {
-		global $config;
-		$servers = array();
-		foreach ($config['tokens'] as $tokenid=>$token) {
-			$feed = "servers.php?auth=".$token;
-			$temp=$this->parseServersFromFeed($feed,$tokenid);
-			foreach ($temp as $server) {
-				array_push($servers,$server);
-			}
-		}
-		return $servers;
-	}
-
-	/*
-	 * Metoda cita Gpanel feed, parsira i vraca niz servera sa informacijama
-	 */
-	function parseServersFromFeed($feed,$tokenid) {
-		global $config;
-		$br_info = 7;
-		$i=0;
-		$dump = $this->procitaj($config['apilink'].$feed);
-		$info = explode("\n",$dump);
-
-		if ($info[0]==0) $this->izbaciGresku($info[1]);
-
-		$brojServera = $info[1];
-
-		for ($j=1;$j<=$brojServera;$j++) {
-		
-			if(in_array($info[(($j-1)*$br_info)+2+0], $config['ignorisi']))
-				continue;
-			
-			$server[$i]['serverid']		=	$info[(($j-1)*$br_info)+2+0];
-			$server[$i]['name']			=	$info[(($j-1)*$br_info)+2+1];
-			$server[$i]['game']			=	$info[(($j-1)*$br_info)+2+2];
-			$server[$i]['ip']			=	$info[(($j-1)*$br_info)+2+3];
-			$server[$i]['slots']		=	$info[(($j-1)*$br_info)+2+4];
-			$server[$i]['online']		=	$info[(($j-1)*$br_info)+2+6];
-			$server[$i]['auth']			=	$tokenid;
-
-			$i++;
-		}
-		return $server;
-	}
-
-	/*
-	 * Metoda vrati ceo sadrzaj datog linka
-	 */
-	function procitaj($link) {
-		global $config;
-
-		if ($config['feedVrsta']==1) {
-			ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)'); 	// laziranje user agenta
-			$podaci=file_get_contents($link);
-
-		} else if ($config['feedVrsta']==2) {
-			
-			if(!function_exists('curl_init'))
-				$this->izbaciGresku("Nemate cURL na web serveru!<br />U config.php promenite feedVrsta!");
-		
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $link);
-			curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11");
-			curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$podaci = curl_exec($curl);
-			curl_close($curl);
-
-		} else $this->izbaciGresku("Nepoznata vrsta feed-a!");
-
-
-
-		if (is_bool($podaci) && $podaci == false) {
-			$error = "Doslo je do greske!<br />Nije primljen nikakav sadrzaj sa linka:<br /><strong>{$link}</strong>";
-			// prolazi kroz sve tokene i uklanja iz linka kako posetioci ne bi mogli da ga vide
-			foreach ($config['tokens'] as $id=>$token) {
-				$error = str_replace($token,"[token{$id} uklonjen]",$error);
-			}
-			$this->izbaciGresku($error);
-		} else {
-			return $podaci;
-		}
-	}
-
-	/*
-	 * Metoda restartuje server pozivom KGB API-ja
+	 * Metoda restartuje server pozivom NERDS API-ja
 	 */
 	function restartujServer($serverid,$token) {
 		global $config;
-		$server_process = "server_process.php?task=restart&auth=$token&id=$serverid";
-		$ret = $this->procitaj($config['apilink'].$server_process);
-		//$info = explode("\n",$ret);
-		return true;
-	}
-
-	/*
-	 * metoda skenira server na GT.rs i vraca informacije
-	 */
-	function skenirajServer($ip) {
-		global $config;
-		$br_skeniranja = $config['broj_skeniranja'];
-		// $link = "ip={$ip}";
-		while (($br_skeniranja--)>0) {
-			$server = json_decode($this->procitaj($config['gtapilink']."/".$ip), true);
-			if ($server['apiError']) return false;
-			if ($server['status']) break;
-
-			sleep(1);
+		$api = $config['apilink']."api.php?token=".$token."&task=restart&id=".$serverid;
+		$data = $data = file_get_contents($api);
+		$serverprocess = json_decode($data);
+		if($serverprocess[0]->status=="ok")
+		{
+			return true;
+		} else {
+			return false;
 		}
-		return $server;
 	}
 
 	/*
-	 * Sigurnosna metoda, iz IP adrese izbaci sve sto nije broj, tacka ili dvotacka
+	 * metoda skenira server na Nerds API 
 	 */
-	function ocistiIP($ip) {
-		return preg_replace("[^0-9\.\:]", "", $ip );
+	function skenirajServer($serverid, $token) {
+		global $config;
+		$api = $config['apilink'].'api.php?token=$token';
+		$data = file_get_contents($api); 
+		$br_skeniranja = $config['broj_skeniranja'];
+		while (($br_skeniranja--)>0) {
+			$server = json_decode($data);
+			$status = $server[0]->status;
+			if($status == 0)
+			{
+				return "Offline";
+			} else {
+			  	return "Online";
+			}
+		}
 	}
 
-	/*
-	 * Metoda ocisti output buffer, a zatim ispise gresku
-	 */
-	function izbaciGresku($tekst) {
-		@ob_clean();
-		$out =
-'<div style="margin:auto;padding:0px 30px 30px 30px;width:800px;border:2px solid blue;">
-<h3>Greska</h3>
-'.$tekst.'
-</div>';
-		die($out);
-	}
 }
 
 $restarter = new Restarter();
